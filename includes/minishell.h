5118f1a91c9cfbf96a70f2f5a1fe76004fed918a
Mathieu <mlecherb@c4r2p4.42nice.fr>
2022-03-31 13:21:02 +0200

    Includes

---
+++ includes/minishell.h
@@ -0,0 +1,22 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
+# include <termcap.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
+# include <errno.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <sys/ioctl.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
