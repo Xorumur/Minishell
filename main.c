3b8e5e33c13ef43c2cc78036f4573f264acdea26
Edracoon <77042040+Edracoon@users.noreply.github.com>
2022-03-31 19:19:49 +0200

    changes

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include "includes/minishell.h"
 
+void minishell()
+{
+	
+}
+
 int main(int argc, char **argv, char **env)
 {
-    (void)argc;
-    (void)env;
-    (void)argv;
+	(void)argc;
+	(void)env;
+	(void)argv;
 
 	while (1)
 	{
 		minishell();
 	}
-} 
+}
